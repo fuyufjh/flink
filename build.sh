245c03e90e73e58ea473155c79dec7d36d8387c6
Eric Fu <eric.fy@alibaba-inc.com>
2019-08-28 10:07:15 +0800

    add my build script

---
+++ build.sh
@@ -0,0 +1,7 @@
+mvn install -pl "!flink-runtime-web" -T8 -Dmaven.test.skip -Dcheckstyle.skip -Drat.skip=true
+
+# archive and copy to my downloads folder
+pushd flink-dist/target/flink-1.9-tpcds-master-bin/
+tar --exclude=flink-1.9-tpcds-master/opt/ -zcvf flink-1.9-tpcds-master.tar.gz flink-1.9-tpcds-master/
+popd
+mv flink-dist/target/flink-1.9-tpcds-master-bin/flink-1.9-tpcds-master.tar.gz ~/Downloads/
